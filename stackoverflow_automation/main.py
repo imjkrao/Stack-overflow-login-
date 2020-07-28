9cfc663e4532a56aff07961f52e19090dd84eb3c
Jeevankrishna <raojk33@gmail.com>
2020-07-28 12:42:22 +0100

    commit

---
+++ stackoverflow_automation/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 11:24:21 2020
+
+@author: jeevan
+"""
+
+import code_selenium
+
+code_selenium.stackoverflow_Login('enter email', 'enter pass')
+
