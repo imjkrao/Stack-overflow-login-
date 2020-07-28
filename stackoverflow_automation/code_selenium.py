be982ac8f2e8d0baa7e1ab95ffbd2541fbe76fa2
Jeevankrishna <raojk33@gmail.com>
2020-07-28 10:34:20 +0100

    enter email
    open stack overflow login page and enter email id

---
+++ stackoverflow_automation/code_selenium.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 10:21:25 2020
+
+@author: jeevan
+"""
+
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument("--test-type")
+driver = webdriver.Chrome(chrome_options=options)
+driver.get('https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f')
+
+driver.maximize_window()
+
+email=driver.find_element_by_name('email')
+email.send_keys("raojk33@hotmail.com")
