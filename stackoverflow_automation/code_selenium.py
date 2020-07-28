f79ff3a778be9d0fb6c238bc516306d244b90829
Jeevankrishna <raojk33@gmail.com>
2020-07-28 11:12:38 +0100

    Update code_selenium.py
    Login to stack overflow feature is added and also converted it to a function called stackoverflow_Login(mail,passw)
    
    Co-Authored-By: Shravan P Rao <spr0107@users.noreply.github.com>
    Co-Authored-By: Anoop Krishna <anoopkrishnarao@users.noreply.github.com>

--- stackoverflow_automation/code_selenium.py
+++ stackoverflow_automation/code_selenium.py
@@ -5,25 +5,30 @@ Created on Tue Jul 28 10:21:25 2020
 @author: jeevan
 """
 
-from selenium import webdriver
-
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument("--test-type")
-driver = webdriver.Chrome(chrome_options=options)
-driver.get('https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f')
-
-driver.maximize_window()
-
-#mail="" #enter your email_id
-#passw="" #enter password
-
-#key user email
-email=driver.find_element_by_name('email')
-email.send_keys(mail)
-
-#key user password
-password=driver.find_element_by_name('password')
-password.send_keys(passw)
-
+def stackoverflow_Login(mail,passw):
+    from selenium import webdriver
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--test-type")
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get('https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f')
+    
+    driver.maximize_window()
+    
+    #mail="" #enter your email_id
+    #passw="" #enter password
+    
+    #key user email
+    email=driver.find_element_by_name('email')
+    email.send_keys(mail)
+    
+    #key user password
+    password=driver.find_element_by_name('password')
+    password.send_keys(passw)
+    
+    #press login
+    login=driver.find_element_by_name('submit-button')
+    driver.implicitly_wait(500)
+    login.click()
 
